094317d9aafe2d0f104f8e0848d2e614983b9c42
Blazej Kepa <blazej.kepa@partner.doubleslash.de>
2022-06-09 14:23:08 +0200

    Swagger docs + asset results pagination

--- src/main/java/net/catenax/traceability/api/TraceabilityController.java
+++ src/main/java/net/catenax/traceability/api/TraceabilityController.java
@@ -4,14 +4,27 @@ import net.catenax.traceability.assets.Asset;
 import net.catenax.traceability.assets.AssetRepository;
 import net.catenax.traceability.assets.PageResult;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
 public class TraceabilityController {
+
+	public record Invitation(String id, Instant createdDate, String name, String surname, String email, String phoneNumber) {
+		public Invitation(Instant createdDate, String name, String surname, String email, String phoneNumber) {
+			this(UUID.randomUUID().toString(), createdDate, name, surname, email, phoneNumber);
+		}
+	}
 
 	private AssetRepository assetRepository;
 
@@ -32,5 +45,22 @@ public class TraceabilityController {
 	@GetMapping("/assets/{assetId}/children/{childId}")
 	public Asset asset(@PathVariable String assetId, @PathVariable String childId) {
 		return assetRepository.getAssetByChildId(assetId, childId);
+	}
+
+	@PutMapping("/invite/{id}")
+	void inviteUser(@PathVariable String id, @RequestBody Invitation request) {
+	}
+
+	@DeleteMapping("/invite/{id}")
+	void removeInvitation(@PathVariable String id) {
+	}
+
+	@PostMapping("/invite")
+	void inviteUser(@RequestBody Invitation invitation) {
+	}
+
+	@GetMapping("/invitations")
+	PageResult<Invitation> getInvitations(Pageable pageable) {
+		return null;
 	}
 }
