e9e2004c3164384fcd42b32b7fda00075e2190b2
Blazej Kepa <blazej.kepa@partner.doubleslash.de>
2022-06-09 14:32:06 +0200

    Remove of unused code

--- src/main/java/net/catenax/traceability/api/TraceabilityController.java
+++ src/main/java/net/catenax/traceability/api/TraceabilityController.java
@@ -4,27 +4,14 @@ import net.catenax.traceability.assets.Asset;
 import net.catenax.traceability.assets.AssetRepository;
 import net.catenax.traceability.assets.PageResult;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.Instant;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
 public class TraceabilityController {
-
-	public record Invitation(String id, Instant createdDate, String name, String surname, String email, String phoneNumber) {
-		public Invitation(Instant createdDate, String name, String surname, String email, String phoneNumber) {
-			this(UUID.randomUUID().toString(), createdDate, name, surname, email, phoneNumber);
-		}
-	}
 
 	private AssetRepository assetRepository;
 
@@ -47,20 +34,4 @@ public class TraceabilityController {
 		return assetRepository.getAssetByChildId(assetId, childId);
 	}
 
-	@PutMapping("/invite/{id}")
-	void inviteUser(@PathVariable String id, @RequestBody Invitation request) {
-	}
-
-	@DeleteMapping("/invite/{id}")
-	void removeInvitation(@PathVariable String id) {
-	}
-
-	@PostMapping("/invite")
-	void inviteUser(@RequestBody Invitation invitation) {
-	}
-
-	@GetMapping("/invitations")
-	PageResult<Invitation> getInvitations(Pageable pageable) {
-		return null;
-	}
 }
