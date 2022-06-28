c51235c06c792842d16c2f740f4218b292897f64
ds-ext-jserwa <104501924+ds-ext-jserwa@users.noreply.github.com>
2022-06-28 19:33:46 +0100

    TRACEFOSS-213: Fix merge conflicts

--- src/main/java/net/catenax/traceability/config/SecurityConfig.java
+++ src/main/java/net/catenax/traceability/config/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 		super.configure(http);
 
 		http
+			.cors()
+			.and()
 			.anonymous().disable()
 			.authorizeRequests()
 			.antMatchers(WHITELIST_URLS).permitAll()
