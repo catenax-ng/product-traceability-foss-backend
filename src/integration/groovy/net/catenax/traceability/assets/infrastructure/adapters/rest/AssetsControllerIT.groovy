a64fbc3a3e0714eeba42297d0183adc732b4038d
ds-ext-jserwa <104501924+ds-ext-jserwa@users.noreply.github.com>
2022-10-19 08:08:11 +0200

    TRACEFOSS-405: Added Asset detail information endpoint

--- src/integration/groovy/net/catenax/traceability/assets/infrastructure/adapters/rest/AssetsControllerIT.groovy
+++ src/integration/groovy/net/catenax/traceability/assets/infrastructure/adapters/rest/AssetsControllerIT.groovy
@@ -457,4 +457,56 @@ class AssetsControllerIT extends IntegrationSpecification implements IrsApiSuppo
 				.statusCode(200)
 				.body("qualityType", equalTo("Critical"))
 	}
+
+	def "should not return assets detail information when user is not authenticated"() {
+		given:
+			cachedBpnsForDefaultAssets()
+
+		and:
+			defaultAssetsStored()
+
+		expect:
+			given()
+				.contentType(ContentType.JSON)
+				.body(
+					asJson(
+						[
+							assetIds: []
+						]
+					)
+				)
+				.when()
+				.post("/api/assets/detail-information")
+				.then()
+				.statusCode(401)
+	}
+
+	def "should return assets detail information"() {
+		given:
+			cachedBpnsForDefaultAssets()
+
+		and:
+			defaultAssetsStored()
+
+		expect:
+			given()
+				.contentType(ContentType.JSON)
+				.body(
+					asJson(
+						[
+							assetIds: [
+								"urn:uuid:fe99da3d-b0de-4e80-81da-882aebcca978",
+								"urn:uuid:d387fa8e-603c-42bd-98c3-4d87fef8d2bb",
+								"urn:uuid:0ce83951-bc18-4e8f-892d-48bad4eb67ef"
+							]
+						]
+					)
+				)
+				.header(jwtAuthorization(ADMIN))
+				.when()
+				.post("/api/assets/detail-information")
+				.then()
+				.statusCode(200)
+				.body("", Matchers.hasSize(3))
+	}
 }
