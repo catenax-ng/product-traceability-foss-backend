4228510d48823462300947c50f436fa6bea06816
Blazej Kepa <blazej.kepa@partner.doubleslash.de>
2022-10-25 21:19:28 +0200

    Making sonar happy again :)

--- src/main/java/net/catenax/traceability/infrastructure/edc/blackbox/EdcCallbackController.java
+++ src/main/java/net/catenax/traceability/infrastructure/edc/blackbox/EdcCallbackController.java
@@ -2,8 +2,6 @@ package net.catenax.traceability.infrastructure.edc.blackbox;
 
 import net.catenax.traceability.infrastructure.edc.blackbox.cache.EndpointDataReference;
 import net.catenax.traceability.infrastructure.edc.blackbox.cache.InMemoryEndpointDataReferenceCache;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +13,6 @@ import springfox.documentation.annotations.ApiIgnore;
 @RequestMapping("/endpoint-data-reference")
 public class EdcCallbackController {
 
-	private static final Logger logger = LoggerFactory.getLogger(EdcCallbackController.class);
 	private final InMemoryEndpointDataReferenceCache endpointDataReferenceCache;
 
 	public EdcCallbackController(InMemoryEndpointDataReferenceCache endpointDataReferenceCache) {
