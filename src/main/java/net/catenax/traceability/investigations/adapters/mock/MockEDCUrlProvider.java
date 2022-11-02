2e15473fb8f39ccba97f5a1cb5983add81ea8432
ds-ext-jserwa <104501924+ds-ext-jserwa@users.noreply.github.com>
2022-11-02 15:34:53 +0100

    Add mock edc url for BPN A

--- src/main/java/net/catenax/traceability/investigations/adapters/mock/MockEDCUrlProvider.java
+++ src/main/java/net/catenax/traceability/investigations/adapters/mock/MockEDCUrlProvider.java
@@ -15,7 +15,8 @@ public class MockEDCUrlProvider implements EDCUrlProvider {
 		"man-1", "https://consumer-controlplane.dev.demo.ftcpro.co",
 		"BPNL00000003AXS3", "https://consumer-controlplane-3.dev.demo.ftcpro.co",
 		"BPNL00000002AXS2", "https://consumer-controlplane.dev.demo.ftcpro.co",
-		"BPNL00000001AXS1", "https://trace-consumer-controlplane.dev.demo.ftcpro.co"
+		"BPNL00000001AXS1", "https://trace-consumer-controlplane.dev.demo.ftcpro.co",
+		"BPNL00000003CML1", "https://trace-consumer-controlplane.dev.demo.ftcpro.co"
 	);
 
 	@Value("${traceability.bpn}")
