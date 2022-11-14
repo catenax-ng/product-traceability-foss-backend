bdf17f4571cf4fc551fdf6698abd8121e28ad96c
ds-ext-jserwa <104501924+ds-ext-jserwa@users.noreply.github.com>
2022-11-14 09:44:23 +0100

    TRACEFOSS-714: Update EDC url provider urls

--- src/main/java/net/catenax/traceability/investigations/adapters/mock/MockEDCUrlProvider.java
+++ src/main/java/net/catenax/traceability/investigations/adapters/mock/MockEDCUrlProvider.java
@@ -15,10 +15,10 @@ public class MockEDCUrlProvider implements EDCUrlProvider {
 		"man-1", "https://consumer-controlplane.dev.demo.ftcpro.co",
 		"BPNL00000003AXS3", "https://consumer-controlplane-3.dev.demo.ftcpro.co",
 		"BPNL00000002AXS2", "https://consumer-controlplane.dev.demo.ftcpro.co",
-		"BPNL00000001AXS1", "https://trace-x-edc.dev.demo.catena-x.net",
-		"BPNL00000003CML1", "https://trace-x-edc.dev.demo.catena-x.net",
-		"BPNL00000003CNKC", "https://trace-x-test-edc.dev.demo.catena-x.net",
-		"BPNL00000003AYRE", "https://trace-x-test-edc.dev.demo.catena-x.net"
+		"BPNL00000001AXS1", "https://trace-x-edc.dev.demo.catena-x.net/api/v1/data/catalog",
+		"BPNL00000003CML1", "https://tracex-consumer-controlplane.dev.demo.catena-x.net/api/v1",
+		"BPNL00000003CNKC", "http://provider-controlplane:8282",
+		"BPNL00000003AYRE", "https://tracex-test-consumer-controlplane.dev.demo.catena-x.net/api/v1"
 	);
 
 	@Value("${traceability.bpn}")
