fe8f7e6eba381f703dfcd8bf6c48eccc1cac5bf6
ds-ext-jserwa <104501924+ds-ext-jserwa@users.noreply.github.com>
2022-11-17 14:07:54 +0100

    TRACEFOSS-714: Debug EDC

--- src/main/java/net/catenax/traceability/infrastructure/edc/blackbox/EdcController.java
+++ src/main/java/net/catenax/traceability/infrastructure/edc/blackbox/EdcController.java
@@ -4,7 +4,7 @@ import net.catenax.traceability.infrastructure.edc.blackbox.model.EDCNotificatio
 import net.catenax.traceability.investigations.domain.service.InvestigationsReceiverService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
@@ -23,7 +23,7 @@ public class EdcController {
 	/**
 	 * Receiver API call for EDC Transfer
 	 */
-	@PostMapping("/qualitynotifications/receive")
+	@GetMapping("/qualitynotifications/receive")
 	public void qualityNotificationReceive(@RequestBody EDCNotification edcNotification) {
 		logger.info("EdcController [qualityNotificationReceive] notificationId:{}", edcNotification);
 
